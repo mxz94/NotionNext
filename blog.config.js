7ca25d3d0dc2bc29f60451ec4ec84abff72a7702
mx <127198845+mxz94@users.noreply.github.com>
2025-04-02 09:04:36 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -145,7 +145,7 @@ const BLOG = {
   GITBOOK_AUTO_SORT: process.env.NEXT_PUBLIC_GITBOOK_AUTO_SORT || true, // 是否自动按分类名 归组排序文章；自动归组可能会打乱您Notion中的文章顺序
 
   GITBOOK_LATEST_POST_RED_BADGE:
-    process.env.NEXT_PUBLIC_GITBOOK_LATEST_POST_RED_BADGE || true, // 是否给最新文章显示红点
+    process.env.NEXT_PUBLIC_GITBOOK_LATEST_POST_RED_BADGE || false, // 是否给最新文章显示红点
 
   // 菜单
   GITBOOK_MENU_CATEGORY: true, // 显示分类
@@ -154,7 +154,7 @@ const BLOG = {
   GITBOOK_MENU_SEARCH: true, // 显示搜索
 
   // 导航文章自动排他折叠
-  GITBOOK_EXCLUSIVE_COLLAPSE: true, // 一次只展开一个分类，其它文件夹自动关闭。
+  // GITBOOK_EXCLUSIVE_COLLAPSE: true, // 一次只展开一个分类，其它文件夹自动关闭。
 
   // Widget
   GITBOOK_WIDGET_REVOLVER_MAPS:
